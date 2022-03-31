651077c99ffeca2bca6ce58bc20f27d835172ca9
alex <skisk8fmx@gmail.com>
2022-03-31 22:17:19 +0300

    blocks 1

--- chillout_framework/engine/templater.py
+++ chillout_framework/engine/templater.py
@@ -1,12 +1,13 @@
 """Render class"""
 import os.path
-from jinja2 import Template
+from jinja2 import Template, Environment, FileSystemLoader
 
 
 class Templater:
     """
     class to work with templates
     """
+
     def __init__(self, template, folder, **kwargs):
         self.template = template
         self.folder = folder
@@ -17,9 +18,13 @@ class Templater:
         :return: render of template
         """
         # path to template
-        path = os.path.join(self.folder, self.template)
+        # path = os.path.join(self.folder, self.template)
+
+        env = Environment()
+        env.loader = FileSystemLoader(self.folder)
+        template = env.get_template(self.template)
 
         # read full template
-        with open(path, encoding='utf-8') as file:
-            template = Template(file.read())
+        # with open(path, encoding='utf-8') as file:
+        #     template = Template(file.read())
         return template.render(**kwargs)
--- chillout_framework/views.py
+++ chillout_framework/views.py
@@ -21,19 +21,23 @@ class Index(Templater):
         )
 
 
-class About(Templater):
+class MobileApplication(Templater):
     """
-    view for about page
+    view for mobile application page
     """
 
     def __init__(self, **kwargs):
-        self.template = 'index.html'
+        self.template = 'mobile_application.html'
         self.folder = 'templates'
-        self.route = '/about/'
+        self.route = '/mobile_application/'
         super().__init__(self.template, self.folder, **kwargs)
 
     def __call__(self, request):
-        return '200 OK', 'about'
+        return '200 OK', self.render(
+            response=request.get('topics', None),
+            style=request.get('style', None),
+            languages=request.get('languages', None)
+        )
 
 
 class NotFound404:
@@ -42,10 +46,45 @@ class NotFound404:
     """
 
     def __init__(self):
-        self.template = 'index.html'
+        self.template = 'base.html'
         self.folder = 'templates'
         self.route = '/404/'
 
     def __call__(self, request):
         return '404 WHAT', '404 PAGE Not Found'
 
+
+class Login(Templater):
+    """
+    view login page
+    """
+
+    def __init__(self):
+        self.template = 'login.html'
+        self.folder = 'templates'
+        self.route = '/login/'
+
+    def __call__(self, request):
+        return '404 WHAT', self.render(
+            response=request.get('topics', None),
+            style=request.get('style', None),
+            languages=request.get('languages', None)
+        )
+
+
+class Registration(Templater):
+    """
+    view login page
+    """
+
+    def __init__(self):
+        self.template = 'registration.html'
+        self.folder = 'templates'
+        self.route = '/registration/'
+
+    def __call__(self, request):
+        return '404 WHAT', self.render(
+            response=request.get('topics', None),
+            style=request.get('style', None),
+            languages=request.get('languages', None)
+        )
